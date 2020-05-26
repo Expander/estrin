a67fcd629f43609d491ac07b872a41662e34c05d
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-26 16:18:20 +0200

    correcting benchmark output

--- test/test_bench.cpp
+++ test/test_bench.cpp
@@ -64,13 +64,13 @@ void test_bench()
    };
 
    const double time_estrin_f = bench_fn(estrin_f, N_EVALS);
-   const double time_estrin_t = bench_fn(horner_f, N_EVALS);
-   const double time_horner_f = bench_fn(estrin_t, N_EVALS);
+   const double time_estrin_t = bench_fn(estrin_t, N_EVALS);
+   const double time_horner_f = bench_fn(horner_f, N_EVALS);
 
    printf("Evaluations of a polynomial with %lu coefficients\n", N);
    printf("  Horner (C)  : %2.1e ms%s\n", time_horner_f, (time_horner_f < time_estrin_f ? " <--": ""));
    printf("  Estrin (C)  : %2.1e ms%s\n", time_estrin_f, (time_horner_f < time_estrin_f ? "": " <--"));
-   printf("  Estrin (C++): %2.1e ms%s\n", time_estrin_t, (time_horner_f < time_estrin_f ? "": " <--"));
+   printf("  Estrin (C++): %2.1e ms  \n", time_estrin_t);
 }
 
 
