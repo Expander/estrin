894ad21eb2c2aec1fa1208584080f74a0efc5c16
Alexander Voigt <ahgvoigt@gmail.com>
2023-01-03 15:23:16 +0100

    adding missing documentation

--- src/horner.c
+++ src/horner.c
@@ -1,3 +1,12 @@
+/**
+ * Evaluate polynomial P(x) using Horner's scheme.
+ *
+ * @param x variable
+ * @param c coefficients (c[0], c[1], ..., c[len-1])
+ * @param len number of coefficients (length of c array)
+ *
+ * @return value of P(x)
+ */
 double horner(double x, const double* c, int len)
 {
    double p = 0.0;
